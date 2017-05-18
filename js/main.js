218cb2bc00255506a13f4c2ad5f9a085d5b1b079
kjeriar <karla.jeria@gmail.com>
2017-05-18 02:20:44 -0400

    ejercicio trello leccion 20

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+	var agregarForm = document.getElementById("agregarForm");
+	var button = document.getElementById("button");
+	var input = document.getElementById("input");
+	var nuevoForm = document.getElementById("nuevoForm");
+	var contenedor = document.getElementById("contenedor");
+	var contador = 1;
